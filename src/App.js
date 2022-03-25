653147d89e26c6b09be5748b186d3fc1467684c0
typejulio <juliodourado419@gmail.com>
2022-03-25 16:01:13 -0300

    inserindo lib mui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import CssBaseline from '@mui/material/CssBaseline';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { BrowserRouter ,Routes, Route } from 'react-router-dom';
 
-function App() {
+//components
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+//pages
+import Inicial from './pages/Inicial'
+
+const sections = [
+  { title: 'Inicial', url: '/inicial' },
+  { title: 'Design', url: '#' },
+  { title: 'Culture', url: '#' },
+  { title: 'Business', url: '#' },
+  { title: 'Politics', url: '#' },
+  { title: 'Opinion', url: '#' },
+  { title: 'Science', url: '#' },
+  { title: 'Health', url: '#' },
+  { title: 'Style', url: '#' },
+  { title: 'Travel', url: '#' },
+];
+
+const theme = createTheme();
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <ThemeProvider theme={theme}>
+      <div className="App">   
+        <CssBaseline />
+        <Header title="SAFE" sections={sections} />
+          <BrowserRouter>
+            <Routes>
+              <Route path='/inicial' element={<Inicial />}/>
+            </Routes>
+          </BrowserRouter>
+        <Footer title='Footer' description='Footer'/>
+      </div>
+    </ThemeProvider>
+
   );
 }
 
-export default App;
+
